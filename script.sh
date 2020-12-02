0e0ac0d95f6ce3b16dcac8c3ea0dcc2d3e010d9f
mark grayson <mgrayson@cisco.com>
2020-12-02 13:56:43 +0000

    initial commit

---
+++ script.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env zsh
+
+# PYANG comilation script
+
+echo "PYANG Compilation Script"
+
+TMP=$(mktemp)
+dirname="working"
+
+# Copy all YANG models to single directory
+mkdir -p "$dirname"
+cp -n *.yang ./$dirname
+
+# Perform PYANG comilation
+pyang ./$dirname/*.yang 2>&1 | tee $TMP
+# alternative with --lint
+# pyang --lint ./$dirname/*.yang 2>&1 | tee $TMP
+
+OUTPUT=$(cat $TMP)
+
+#tidy up
+rm $TMP
+rm -r ./$dirname
+
+# echo "output is " $OUTPUT
+
+#check for --lint errors
+if [[ $OUTPUT = *error* ]];
+then
+  echo "PYANG compilation failed"
+  exit 1
+else
+  echo "PYANG compilation passed"
+  exit 0
+fi
